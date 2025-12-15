e69a7d394432cb966db60969de02b2365ded29ce
Quaerent <topfyf@qq.com>
2025-12-16 01:09:14 +0800

    Initial commit

---
+++ build.lua
@@ -0,0 +1,11 @@
+module        = "quatex"
+
+sourcefiledir = "code"
+docfiledir    = "doc"
+typesetfiles  = { "*.dtx", "*.tex" }
+packtdszip    = true -- recommended for "tree" layouts
+
+-- typesetting options
+typesetopts = "-interaction=nonstopmode" 
+checkopts   = "-interaction=nonstopmode"
+unpackopts  = "-interaction=nonstopmode"
