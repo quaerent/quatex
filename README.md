4c4d8e31c75034baf9ff8e6fba55b7a719f770c9
Quaerent <topfyf@qq.com>
2026-01-11 21:34:56 +0800

    Update TeXJAM and support cleaner prompts

---
+++ README.md
@@ -0,0 +1,4 @@
+# `QuaTeX`
+
+A LaTeX template made by [Quaerent](https://github.com/quaerent),
+powered by [TeXJam](https://github.com/quaerent/texjam).
--- plugins/module.py
+++ plugins/module.py
@@ -1,47 +1,45 @@
-from texjam import MetaField, TexJamPlugin
-from texjam.path import TempPath
+from texjam import TempPath, TexJamPlugin
+from texjam.config import MetaField
 
 
-class _ModulePlugin(TexJamPlugin):
-    module_name: str
-    meta_fields: list[str] = []
+class ModuleFilterPlugin(TexJamPlugin):
+    modules: list[str] = [
+        'layout',
+        'font',
+        'reference',
+        'citation',
+        'math',
+        'environment',
+    ]
 
-    def pre_prompt(self, field: MetaField) -> bool | None:
-        if (
-            field.key in self.meta_fields
-            and self.metadata.get(f'enable_{self.module_name}') is False
-        ):
-            return True
+    def enabled(self, module_name: str) -> bool:
+        if 'modules' in self.metadata:
+            return module_name in self.metadata['modules']
+        return False
+
+    @property
+    def enabled_modules(self) -> list[str]:
+        if 'modules' in self.metadata:
+            return self.metadata['modules']
+        return []
+
+    def get_module_name(self, path: TempPath) -> str | None:
+        if path.raw is None:
+            return None
+        parts = path.raw.relative_to(self.texjam.template_source_dir).parts
+        if len(parts) < 3 or parts[1] != 'preamble':
+            return None
+        return parts[2]
+
+    def pre_prompt(self, name: str, field: MetaField) -> bool | None:
+        for module_name in self.modules:
+            if name == f'{module_name}_options':
+                return not self.enabled(module_name)
 
     def on_paths(self, paths: list[TempPath]) -> list[TempPath] | None:
-        if self.metadata.get(f'enable_{self.module_name}') is False:
-            return [
-                p
-                for p in paths
-                if p.raw is None
-                or p.raw.relative_to(self.texjam.template_source_dir).parts[2]
-                != self.module_name
-            ]
-
-
-class LayoutPlugin(_ModulePlugin):
-    module_name = 'layout'
-    meta_fields = ['fancy_header_footer']
-
-
-class ReferencePlugin(_ModulePlugin):
-    module_name = 'reference'
-
-
-class CitationPlugin(_ModulePlugin):
-    module_name = 'citation'
-
-
-class EnvironmentPlugin(_ModulePlugin):
-    module_name = 'environment'
-    meta_fields = ['define_custom_environments']
-
-
-class MathPlugin(_ModulePlugin):
-    module_name = 'math'
-    meta_fields = ['display_equation_style', 'define_custom_macros']
+        filtered_paths = []
+        for path in paths:
+            module_name = self.get_module_name(path)
+            if module_name is None or self.enabled(module_name):
+                filtered_paths.append(path)
+        return filtered_paths
